30790ec614a5257c7e20b8cfe96b7ca90bef4cdc
aa <aa>
2017-08-21 17:49:10 +0800

    sss

---
+++ aa/src/aa/aa.java
@@ -0,0 +1,8 @@
+package aa;
+
+public class aa {
+
+	public static void main(String[] args) {
+		
+	}
+}
--- git/src/test/test.java
+++ git/src/test/test.java
@@ -4,7 +4,7 @@ public class test {
 	
 	int a=4;
 	
-	
+	int u=0;
 	public static void main(String[] args) {
 		 
 	}
